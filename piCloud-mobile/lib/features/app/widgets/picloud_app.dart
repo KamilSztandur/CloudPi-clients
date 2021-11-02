60a85a645880b080643c642b1be1e400c71eac2d
KamilSztandur <kamil.sztandur@vp.pl>
2021-11-02 16:53:32 +0100

    Add file explorer feature
    - added file explorer's page.
    - added file explorer's view.
    - added file explorer's widgets.
    - added bloc mock files.

--- piCloud-mobile/lib/features/app/widgets/picloud_app.dart
+++ piCloud-mobile/lib/features/app/widgets/picloud_app.dart
@@ -1,5 +1,5 @@
-import 'package:app/features/app/widgets/app_bar/appbar.dart';
 import 'package:app/features/app/widgets/bottom_navigation_bar.dart';
+import 'package:app/features/file_explorer/presentation/pages/file_explorer_page.dart';
 import 'package:flutter/material.dart';
 
 class PICloudApp extends StatelessWidget {
@@ -12,9 +12,8 @@ class PICloudApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.grey.shade100,
       ),
       home: Scaffold(
-        appBar: PICloudAppBar(height: 50.0),
-        body: Container(),
         bottomNavigationBar: PICloudBottomNavigationBar(),
+        body: FileExplorerPage(),
       ),
     );
   }
