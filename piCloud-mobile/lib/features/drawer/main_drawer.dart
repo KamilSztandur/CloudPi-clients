72d4bc76ca73d66c80a8ec644d3272c4fc1304d4
KamilSztandur <kamil.sztandur@vp.pl>
2021-11-17 17:59:51 +0100

    Refactor drawer
    - improve drawer's behaviour on different screen resolutions.
    - add About button implementation.
    - clean code.

--- piCloud-mobile/lib/features/drawer/main_drawer.dart
+++ piCloud-mobile/lib/features/drawer/main_drawer.dart
@@ -5,83 +5,134 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({Key? key}) : super(key: key);
+  const MainDrawer({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Container(
-            color: Colors.grey[900],
-            child: ListView(
-              children: [
-                Container(
-                    height: 150.0,
-                    margin: EdgeInsets.all(0.0),
-                    padding: EdgeInsets.all(0.0),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            bottom:
-                                BorderSide(width: 0.5, color: Colors.white60))),
-                    child: Center(
-                        child: DrawerHeader(
-                            child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                          Container(
-                              height: 70,
-                              width: 74,
-                              child: UserProfileImage(size: 60)),
-                          Text(
-                            'Adam44', //Mock or sth, should get username from user's data
-                            style: TextStyle(color: Colors.white, fontSize: 28),
-                          )
-                        ])))),
-                MainDrawerItem(
-                    value: 'User Profile',
-                    icon: Icons.account_box_outlined,
-                    onPressed: () => {
-                          //TODO
-                        }),
-                MainDrawerItem(
-                    value: 'Settings',
-                    icon: Icons.settings_outlined,
-                    onPressed: () => {
-                          //TODO
-                        }),
-                MainDrawerItem(
-                    value: 'Cloud Settings',
-                    icon: Icons.admin_panel_settings_outlined,
-                    onPressed: () => {
-                          //TODO
-                        }),
-                MainDrawerItem(
-                    value: 'About',
-                    icon: Icons.info_outline,
-                    onPressed: () => {
-                          //TODO
-                        }),
-                MainDrawerItem(
-                    value: 'Logout',
-                    icon: Icons.logout_outlined,
-                    onPressed: () => {
-                          //TODO
-                        }),
-                Container(
-                    height: 140,
-                    alignment: Alignment.bottomCenter,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text('PiCloud Mobile',
-                              style: TextStyle(
-                                  color: Colors.white60, fontSize: 16)),
-                          Text(Config.version,
-                              style: TextStyle(
-                                  color: Colors.white70, fontSize: 14))
-                        ]))
-              ],
-            )));
+      child: Container(
+        padding: EdgeInsets.all(15.0),
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          children: [
+            _getDrawerHeader(),
+            _getDrawerOptions(context),
+            Spacer(flex: 3),
+            _getVersionInfoLabel(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getDrawerHeader() {
+    return Container(
+      margin: EdgeInsets.all(0.0),
+      padding: EdgeInsets.all(0.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Colors.white60),
+        ),
+      ),
+      child: Center(
+        child: DrawerHeader(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                height: 70,
+                width: 74,
+                child: UserProfileImage(size: 60),
+              ),
+              Text(
+                'Adam44', //Mock or sth, should get username from user's data
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDrawerOptions(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          MainDrawerItem(
+            value: 'User Profile',
+            icon: Icons.account_box_outlined,
+            onPressed: _onUserProfileTapped,
+          ),
+          MainDrawerItem(
+            value: 'Settings',
+            icon: Icons.settings_outlined,
+            onPressed: _onSettingsTapped,
+          ),
+          MainDrawerItem(
+            value: 'Cloud Settings',
+            icon: Icons.admin_panel_settings_outlined,
+            onPressed: _onCloudSettingsTapped,
+          ),
+          MainDrawerItem(
+            value: 'About',
+            icon: Icons.info_outline,
+            onPressed: () => _onAboutTapped(context),
+          ),
+          MainDrawerItem(
+            value: 'Logout',
+            icon: Icons.logout_outlined,
+            onPressed: _onLogoutTapped,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getVersionInfoLabel() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'PiCloud Mobile',
+            style: TextStyle(color: Colors.white60, fontSize: 16),
+          ),
+          Text(
+            Config.version,
+            style: TextStyle(color: Colors.white70, fontSize: 14),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onUserProfileTapped() {
+    //TODO
+  }
+
+  void _onSettingsTapped() {
+    //TODO
+  }
+
+  void _onCloudSettingsTapped() {
+    //TODO
+  }
+
+  void _onAboutTapped(BuildContext context) => showAboutDialog(
+        context: context,
+        applicationName: Config.applicationName,
+        applicationLegalese: Config.applicationLegalese,
+        applicationVersion: Config.version,
+      );
+
+  void _onLogoutTapped() {
+    //TODO
   }
 }
--- piCloud-mobile/lib/features/drawer/main_drawer_item.dart
+++ piCloud-mobile/lib/features/drawer/main_drawer_item.dart
@@ -7,33 +7,38 @@ class MainDrawerItem extends StatelessWidget {
   final IconData? icon;
   final Color? colorOfIcon;
 
-  const MainDrawerItem(
-      {Key? key,
-      required this.value,
-      required this.onPressed,
-      this.icon,
-      this.colorOfIcon})
-      : super(key: key);
+  const MainDrawerItem({
+    Key? key,
+    required this.value,
+    required this.onPressed,
+    this.icon,
+    this.colorOfIcon,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-              EdgeInsets.symmetric(vertical: 20, horizontal: 10))),
-      child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-        Icon(
-          this.icon,
-          color: this.colorOfIcon,
-          size: 30,
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          EdgeInsets.symmetric(vertical: 20, horizontal: 10),
         ),
-        Container(width: 40),
-        Text(
-          ' ' + this.value,
-          style: TextStyle(fontSize: 24),
-        )
-      ]),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            this.icon,
+            color: this.colorOfIcon,
+            size: 30,
+          ),
+          Container(width: 40),
+          Text(
+            ' ' + this.value,
+            style: TextStyle(fontSize: 24),
+          )
+        ],
+      ),
       onPressed: this.onPressed,
     );
   }
