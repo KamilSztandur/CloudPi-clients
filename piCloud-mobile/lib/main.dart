f82e1c589826d067e3d53082f763188d21b432c9
Robert Odrowaz <robert@odrowaz.dev>
2021-11-22 19:48:33 +0100

    Fix navigator state resetting

--- piCloud-mobile/lib/main.dart
+++ piCloud-mobile/lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:app/features/app/widgets/picloud_app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(PICloudApp());
+void main() => runApp(const PICloudApp());
