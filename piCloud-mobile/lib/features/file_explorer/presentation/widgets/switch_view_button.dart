8dd58593d57aa631045627a2ba0d7c0d3b4dd48c
Robert Odrowaz <robert@odrowaz.dev>
2022-03-04 21:58:02 +0100

    Resolve warnings

--- piCloud-mobile/lib/features/file_explorer/presentation/widgets/switch_view_button.dart
+++ piCloud-mobile/lib/features/file_explorer/presentation/widgets/switch_view_button.dart
@@ -2,7 +2,6 @@ import 'package:app/common/models/view_mode.dart';
 import 'package:app/common/preferences/view_mode_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SwitchViewButton extends StatefulWidget {
   const SwitchViewButton({
